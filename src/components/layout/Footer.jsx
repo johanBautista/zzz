e10c385591eda78e448143aadeb39c0b1f68e79e
johanBautista <jsbparra@gmail.com>
2025-01-05 00:26:29 +0100

    other

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <section className="flex justify-between p-4 bg-teal-600 text-white">
+    <section className="flex justify-between p-4 text-primary">
       <h2 className="text-xl font-thin tracking-wider">Johan Bautista <br /> &copy;  {year}</h2>
     </section>
   )
