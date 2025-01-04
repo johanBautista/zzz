5eff025e0c1da78efa75cd05856ce966765d6108
johanBautista <jsbparra@gmail.com>
2025-01-04 20:06:22 +0100

    install sass and push experience section

---
+++ src/components/ExperiencesSection.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import classes from './Experience.module.scss'
+
+const ExperiencesSection = () => {
+  const [selectedTab, setSelectedTab] = useState('experience')
+
+  const educationData = [
+    {
+      name: 'Grado en Multimedia',
+      academy: 'UOC',
+      year: 'En curso',
+      description: 'Grado interdisciplinar, competencias en diseño, programación y medios multimedia.'
+    },
+    {
+      name: 'Bootcamp Web',
+      academy: 'Ironhack',
+      year: '2019',
+      description: 'Formación intensiva para adquirir habilidades técnicas en desarrollo web.'
+    },
+    {
+      name: 'CFGS Sonido',
+      academy: 'IES La Mercé',
+      year: '2013',
+      description: 'Especialización en tratamiento y producción de audio para medios audiovisuales.'
+    },
+    {
+      name: 'CFGS Audiovisuales',
+      academy: 'IES Sta. Eulalia',
+      year: '2010',
+      description: 'Formación técnica en desarrollo y producción de contenidos audiovisuales.'
+    }
+  ]
+
+  const experienceData = [
+    {
+      company: 'Indicator',
+      role: 'Frontend',
+      city: '',
+      date: 'Jun 2023 - Dic 2024',
+      summary: 'Creación de componentes para la construcción de páginas web por medio de un backoffice.'
+    },
+    {
+      company: 'Sirion Developers',
+      role: 'Frontend',
+      city: '',
+      date: 'Mar 2021 - Mar 2023',
+      summary:
+        'Participación en proyectos en sectores inmobiliario, transporte y hostelería. Maquetación píxel perfect y desarrollo agile.'
+    }
+  ]
+
+  const data = selectedTab === 'experience' ? experienceData : educationData
+  return (
+    <section className={classes.experience}>
+      <div className='w-full mx-auto flex justify-center'>
+        <div className="w-1/4 flex items-center">
+          <span>
+            🧗🏼‍♂️
+          </span>
+          <h2 className='text-indigo-500/75 font-semibold tracking-wider sm:text-end px-1'>
+            ROADMAP
+          </h2>
+        </div>
+      </div>
+
+      <div className={classes['experience-section']}>
+        <div className={classes['experience-section__switch']}>
+          <button
+            className={`${classes['experience-section__tab']} ${selectedTab === 'experience' ? classes['experience-section__tab--active'] : ''
+              }`}
+            onClick={() => setSelectedTab('experience')}
+          >
+            Experiencia
+          </button>
+          <button
+            className={`${classes['experience-section__tab']} ${selectedTab === 'education' ? classes['experience-section__tab--active'] : ''
+              }`}
+            onClick={() => setSelectedTab('education')}
+          >
+            Educación
+          </button>
+        </div>
+
+        <div className={classes['experience-section__timeline']}>
+          {data.map((item, index) => (
+            <div
+              className={`${classes['experience-section__item']} ${index % 2 === 0 ? classes['experience-section__item--left'] : classes['experience-section__item--right']
+                }`}
+              key={index}
+            >
+              <div className={classes['experience-section__dot']} />
+              <div className={classes['experience-section__content']}>
+                {selectedTab === 'education'
+                  ? (
+                    <>
+                      <h3 className={classes['experience-section__title']}>{item.name}</h3>
+                      <div className={classes['experience-section__subcontent']}>
+                        <p className={classes['experience-section__subtitle']}>{item.academy}</p>
+                        <p className={classes['experience-section__year']}>{item.year}</p>
+                      </div>
+                      <p className={classes['experience-section__description']}>{item.description}</p>
+                    </>
+                  )
+                  : (
+                    <>
+                      <h3 className={classes['experience-section__title']}>{item.company}</h3>
+                      <div className={classes['experience-section__subcontent']}>
+                        <p className={classes['experience-section__subtitle']}>{item.role}</p>
+                        <p className={classes['experience-section__year']}>{item.date}</p>
+                      </div>
+                      <p className={classes['experience-section__description']}>{item.summary}</p>
+                    </>
+                  )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ExperiencesSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import HeroSection from '@components/HeroSection'
 import AboutSection from '@components/AboutSection'
+import ExperiencesSection from '@components/ExperiencesSection'
 
 export default function Home () {
   return (
-      <div className="mx-4 sm:px-6 md:px-8 lg:px-6 xl:px-12 2xl:px-20 font-light">
-        <HeroSection />
-        <AboutSection />
-      </div>
+    <div className="mx-4 sm:px-6 md:px-8 lg:px-6 xl:px-12 2xl:px-20 font-light">
+      <HeroSection />
+      <AboutSection />
+      <ExperiencesSection />
+    </div>
   )
 }
